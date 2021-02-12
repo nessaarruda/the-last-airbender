c425f6d338fcff03e603d9811da97bb43dcb6653
Vanessa Arruda <wanessaarruda@gmail.com>
2021-02-12 09:02:32 -0700

    Build connection, response and parse members in controller. Not fully functional yet

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,17 @@
 class SearchController < ApplicationController
 
   def index
-    
+    conn = Faraday.new('https://last-airbender-api.herokuapp.com/api/v1/')
+    response = conn.get('characters?affiliation=Fire+Nation')
+    @members = JSON.parse(response.body, symbolize_keys: true)
+    @members.each do |member|
+      # number_of_members = probably limitation issue that is returing only 20
+      name = member['name']
+      photo = members['photoUrl']
+      allies = member['allies']
+      enemies = member['enemies']
+      affiliations = members['affiliation']
+      require "pry"; binding.pry
+    end
   end
 end
