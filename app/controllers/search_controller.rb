07a07b71ff794d0af7b5296b3f0c25737bc0bf4f
Vanessa Arruda <wanessaarruda@gmail.com>
2021-02-12 09:24:50 -0700

    Take a step back, concat will not be the best solution

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,13 +2,7 @@ class SearchController < ApplicationController
 
   def index
     conn = Faraday.new('https://last-airbender-api.herokuapp.com/api/v1/')
-    response1 = conn.get('characters?affiliation=Fire+Nation&page=1')
-    response2 = conn.get('characters?affiliation=Fire+Nation&page=2')
-    response3 = conn.get('characters?affiliation=Fire+Nation&page=3')
-    response4 = conn.get('characters?affiliation=Fire+Nation&page=4')
-    response5 = conn.get('characters?affiliation=Fire+Nation&page=5')
-    final_response = 
-
+    response = conn.get('characters?affiliation=Fire+Nation')
     @members = JSON.parse(response.body, symbolize_keys: true)
     @members.each do |member|
       # number_of_members = probably limitation issue that is returing only 20
