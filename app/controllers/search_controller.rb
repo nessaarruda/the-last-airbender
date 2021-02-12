feb49fa8549c066c14c2ce10507a6b11f810027d
Vanessa Arruda <wanessaarruda@gmail.com>
2021-02-12 10:49:37 -0700

    Unable to integrate the poro, still using keys in the view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,6 +3,7 @@ class SearchController < ApplicationController
   def index
     nation = 'Fire Nation'# need to use params[:nation]
     @members = MembersFacade.get_member(nation)
+    require "pry"; binding.pry
     # @members = MembersFacade.all_members
     # @members.each do |member|
     #   @count = @members.size
