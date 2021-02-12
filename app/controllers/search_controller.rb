51feb1544305b9e8dbbbe6ce0e76cfe9bf4ec725
Vanessa Arruda <wanessaarruda@gmail.com>
2021-02-12 09:38:30 -0700

    Abandon loop approach

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,32 +1,29 @@
 class SearchController < ApplicationController
 
   def index
-    conn = Faraday.new('https://last-airbender-api.herokuapp.com/api/v1/')
-    response = conn.get('characters?affiliation=Fire+Nation')
-    @members = JSON.parse(response.body, symbolize_keys: true)
-    @members.each do |member|
-      # number_of_members = probably limitation issue that is returing only 20
+    all_pages.each do |member|
+      number_of_members = all_pages.size
       name = member['name']
       photo = member['photoUrl']
       allies = member['allies']
       enemies = member['enemies']
       affiliations = member['affiliation']
-      require "pry"; binding.pry
     end
   end
 
   def all_pages
     counter = 5
     page = 1
-    final_result = []
+    @all_members = []
     while counter != 0 do
     conn = Faraday.new('https://last-airbender-api.herokuapp.com/api/v1/')
     response = conn.get("characters?affiliation=Fire+Nation?page=#{page}")
-    final_result << response
+    body = JSON.parse(response.body, symbolize_keys: true)
+    @all_members << body
     counter -= 1
     page += 1
-    end
     require "pry"; binding.pry
-    @members = JSON.parse(response.body, symbolize_keys: true)
+    end
+    @all_members
   end
 end
